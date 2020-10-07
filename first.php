0506ce751d52ee09bd5c0dbc1f6518266867153d
mikac17 <56652463+mikac17@users.noreply.github.com>
2020-10-07 23:17:29 +0200

    Add files via upload

---
+++ first.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>prva php stranica</title>
+</head>
+<body>
+    
+       <!-- Script 1.2 - first.php -->
+       <p>
+           Miloš <br> Ikač <br>S01/17
+       </p>
+       
+       <?php
+       ?>
+
+</body>
+</html>
